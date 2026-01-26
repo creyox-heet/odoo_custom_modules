1b80eb4d07c29dcada7f4897ab82529c8b10b55b
Heet <heet.creyox@gmail.com>
2026-01-26 12:14:32 +0530

    [UPDATE]implement related for get email in student model

--- school_management/models/student_leave.py
+++ school_management/models/student_leave.py
@@ -5,5 +5,8 @@ class StudentLeave(models.Model):
     _description = "Student Leave"
 
     student_id = fields.Many2one('student.student', string="Student")
+    student_name = fields.Char(string="unused",related="student_id.name")
+
+    stud_email = fields.Char(string="email", related="student_id.email")
     date = fields.Date(string="Date")
     reason = fields.Char(string="Reason")
